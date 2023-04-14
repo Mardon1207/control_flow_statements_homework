853700435d9394d91688c418b5c1c240426f9cc5
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-14 11:18:41 +0500

    6

--- if06.py
+++ if06.py
@@ -13,4 +13,26 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    x=0 
+    y=0
+    if a<0:
+        x=x+1
+    if b<0:
+        x=x+1
+    if c<0:
+        x=x+1
+    if a>0:
+        y=y+1
+    if b>0:
+        y=y+1
+    if c>0:
+        y=y+1
+    if x<y:
+        l=str("there are a lot of positive numbers")
+    if x>y:
+        l=str("there are a lot of negative numbers")
+    return l
+a=int(input())
+b=int(input())
+c=int(input())
+print(main(a,b,c))   
